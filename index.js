16d6760c4a8687778f99db36aca9d577b466bbb1
tamag3221 <tamag3221@gmail.com>
2026-03-15 14:33:17 +0900

    Update postMinute to 43

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const postDays = [1,3,0]; // 月 水 日
 
 // 投稿時間
 const postHour = 14;
-const postMinute = 30;
+const postMinute = 43;
 
 // GAS Webhook（あとで自分のURLに変更）
 const gasWebhook = "https://script.google.com/macros/s/AKfycbzo1jFM4vXzn6-3OpObB7VDZZNHP4lj0FNTsNVeWUdiyCR3hMs7Qn5IpMdSr3gm9P0O/exec";
